246a4fa8b5ae38ce93e9aa9fdf584ed046d5c5b9
e <1041329432@qq.com>
2020-03-31 11:15:21 +0800

    -n

---
+++ design_pattern/src/main/java/com/design/demo/DesignModalApplication.java
@@ -0,0 +1,13 @@
+package com.design.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModalApplication.class, args);
+    }
+
+}
