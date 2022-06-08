cb2b31dc6fca920442d8e5556a3a343d192c5259
e <1041329432@qq.com>
2022-06-09 00:40:15 +0800

    历史提交，算法学习init

--- java-base/src/main/java/com/example/excel/ExcelUtils.java
+++ java-base/src/main/java/com/example/excel/ExcelUtils.java
@@ -3,7 +3,16 @@ package com.example.excel;
 /**
  * @Auther: 10413
  * @Date: 2021-09-03 19:10
- * @Description:
+ * @Description: Excel 文件操作
  */
 public class ExcelUtils {
+
+    public static void main(String[] args) {
+        // 设定Excel文件所在路径
+        String excelFileName = "/Users/Dreamer-1/Desktop/myBlog/java解析Excel/readExample.xlsx";
+        // 读取Excel文件内容
+//        List<ExcelDataVO> readResult = ExcelReader.readExcel(excelFileName);
+
+        // todo 进行业务操作
+    }
 }
